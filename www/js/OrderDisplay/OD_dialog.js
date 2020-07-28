f097fb91ef9800e4cfcb9083a6c1d377ede69020
NaotoYasuoka <yasupon.out@gmail.com>
2020-07-28 22:36:08 +0900

    dialogの修正

--- www/js/OrderDisplay/OD_dialog.js
+++ www/js/OrderDisplay/OD_dialog.js
@@ -1,12 +1,14 @@
 // This is a JavaScript file
 
 function OD_selectNum(id){
-  var splitID = id.split("_");
+  const splitID = id.split("_");
+  let selectNum;
 
   if(splitID[0] == "ODButton"){
     selectNum = Number(splitID[1]);
   }else{
     selectNum = Number(document.getElementById("OD_textbox").value);
   }
+  alert(selectNum);
 }
 
